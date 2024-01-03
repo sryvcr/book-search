d83c07e2ff3310d9ea9d6e9527c52ef1f212100b
sryvcr <andresgx9@gmail.com>
2024-01-03 18:01:47 -0500

    feat: add BookSerializer

---
+++ src/apps/library_admin/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+
+class BookSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    title = serializers.ReadOnlyField()
+    subtitle = serializers.ReadOnlyField()
+    authors = serializers.ReadOnlyField()
+    categories = serializers.ReadOnlyField()
+    publication_date = serializers.ReadOnlyField()
+    editor = serializers.ReadOnlyField()
+    description = serializers.ReadOnlyField()
+    image = serializers.ReadOnlyField()
