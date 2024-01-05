0dcc9c5dad26709ca3e523d4f96ba7c1de8eeab4
sryvcr <andresgx9@gmail.com>
2024-01-05 00:32:09 -0500

    update: add order_by title to get categories providers

--- src/apps/library_admin/providers/category.py
+++ src/apps/library_admin/providers/category.py
@@ -5,7 +5,9 @@ from apps.library_admin.exceptions import CategoryDoesNotExist
 
 
 def get_book_category_names_by_book_id(book_id: int) -> QuerySet:
-    return Category.objects.filter(book__id=book_id).values_list("name", flat=True)
+    return Category.objects.filter(
+        book__id=book_id
+    ).order_by("name").values_list("name", flat=True)
 
 
 def get_category_by_category_name(category_name: str) -> bool:
