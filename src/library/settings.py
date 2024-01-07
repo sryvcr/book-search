a1666578dc16eed1f0967c1704a3f91324d0ca92
sryvcr <andresgx9@gmail.com>
2024-01-06 22:12:57 -0500

    feat: add config to use async functions inside the project

--- src/library/settings.py
+++ src/library/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "adrf",
     "rest_framework",
     "apps.library_admin",
 ]
